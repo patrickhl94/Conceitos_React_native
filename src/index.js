c92402ada06bea97be2008b20fe3fe62b55f68ca
Patrick Lima <patrickhl94@hotmail.com>
2020-04-14 23:09:30 -0300

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, StatusBar } from 'react-native'
 
 export default function App() {
 
   return (
-    <View>
-      <Text>
+    <>
+    <StatusBar backgroundColor='#7159c1' barStyle='light-content' />
+    <View style={styles.container} >
+      <Text style={styles.text} >
         Hello Word
       </Text>
-    </View>)
+    </View>
+    </>
+  )
 }
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  text: {
+    color: '#fff',
+    fontSize: 25,
+    fontWeight: 'bold'
+  }
+
+})
